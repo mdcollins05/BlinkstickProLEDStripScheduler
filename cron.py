e4414b3629ab2ff0c86a397174047fe603a5e957
Matt Collins <matt@m-collins.com>
2021-05-20 09:25:16 -0700

    Fix naming of functions and variables

--- cron.py
+++ cron.py
@@ -10,88 +10,88 @@ import yaml
 from colour import Color
 from blinkstick import blinkstick
 
-time_regex = re.compile("^[0-9]{1,2}:[0-9]{2}$")
-timespan_regex = re.compile("^[0-9]{1,2}:[0-9]{2}-[0-9]{1,2}:[0-9]{2}$")
+timeRegex = re.compile("^[0-9]{1,2}:[0-9]{2}$")
+timespanRegex = re.compile("^[0-9]{1,2}:[0-9]{2}-[0-9]{1,2}:[0-9]{2}$")
 
-def loadschedule(configpath):
-	configfile = open(configpath)
-	data = yaml.safe_load(configfile)
-	configfile.close()
+def load_schedule(configpath):
+	configFile = open(configpath)
+	data = yaml.safe_load(configFile)
+	configFile.close()
 	return data
 
-def naturalgradient(start, end, steps):
+def natural_gradient(start, end, steps):
 	return list(Color(start).range_to(Color(end), steps))
 
-def lineargradient(start, end, steps):
+def linear_gradient(start, end, steps):
 	'''returns a gradient list of (n) colors between
-	  two hex colors. start_hex and finish_hex
+	  two hex colors. startHex and endHex
 	  should be the full six-digit color string,
 	  inlcuding the number sign ("#FFFFFF")
 	  Original from https://bsou.io/posts/color-gradients-with-python
 	  Modified for my purposes'''
 
-	start_hex = hextoRGB(Color(start).hex_l)
-	end_hex = hextoRGB(Color(end).hex_l)
+	startHex = hex_to_RGB(Color(start).hex_l)
+	endHex = hex_to_RGB(Color(end).hex_l)
 
 	# Initilize a list of the output colors with the starting color
-	RGB_list = [RGBtohex(start_hex)]
+	RGBList = [RGB_to_hex(startHex)]
 
 	# Calcuate a color at each evenly spaced value of t from 1 to steps
 	for step in range(1, steps):
 		# Interpolate RGB vector for color at the current value of t
-		curr_vector = [
-			int(start_hex[j] + (float(step)/(steps-1))*(end_hex[j]-start_hex[j]))
+		currVector = [
+			int(startHex[j] + (float(step)/(steps-1))*(endHex[j]-startHex[j]))
 			for j in range(3)
 		]
 		# Add it to our list of output colors
-		RGB_list.append(RGBtohex(curr_vector))
+		RGBList.append(RGB_to_hex(currVector))
 
-	return RGB_list
+	return RGBList
 
-def hextoRGB(hex):
+def hex_to_RGB(hex):
 	color = Color(hex)
 	return (int(color.red * 255), int(color.green * 255), int(color.blue * 255))
 
-def RGBtohex(rgb):
+def RGB_to_hex(rgb):
 	color = Color(rgb=(rgb[0]/float(255), rgb[1]/float(255), rgb[2]/float(255)))
 	return color.hex_l
 
-def getcurrentscheduleentries(config, time):
+def get_current_schedule_entries(config, time):
 	entries = []
 	for entry in config['schedule']:
-		if time_regex.match(entry['time']):
-			split_entry_time = entry['time'].split(":")
-			time_to_compare = int(split_entry_time[0])*60 + int(split_entry_time[1])
-			if time == time_to_compare:
+		if timeRegex.match(entry['time']):
+			splitEntryTime = entry['time'].split(":")
+			timeToCompare = int(splitEntryTime[0])*60 + int(splitEntryTime[1])
+			if time == timeToCompare:
 				entries.append(entry)
 
-		if timespan_regex.match(entry['time']):
-			split_entry_times = entry['time'].split("-")
-			split_entry_time_start = split_entry_times[0].split(":")
-			split_entry_time_end = split_entry_times[1].split(":")
-			time_to_compare_start = int(split_entry_time_start[0])*60 + int(split_entry_time_start[1])
-			time_to_compare_end = int(split_entry_time_end[0])*60 + int(split_entry_time_end[1])
-			if time >= time_to_compare_start and time <= time_to_compare_end:
-				new_entry = dict(entry)
-				new_entry['start'] = time_to_compare_start
-				new_entry['end'] = time_to_compare_end
-				entries.append(new_entry)
+		if timespanRegex.match(entry['time']):
+			splitEntryTimes = entry['time'].split("-")
+			splitEntryTimeStart = splitEntryTimes[0].split(":")
+			splitEntryTimeEnd = splitEntryTimes[1].split(":")
+			timeToCompareStart = int(splitEntryTimeStart[0])*60 + int(splitEntryTimeStart[1])
+			timeToCompareEnd = int(splitEntryTimeEnd[0])*60 + int(splitEntryTimeEnd[1])
+			if time >= timeToCompareStart and time <= timeToCompareEnd:
+				newEntry = dict(entry)
+				newEntry['start'] = timeToCompareStart
+				newEntry['end'] = timeToCompareEnd
+				entries.append(newEntry)
 	return entries
 
-def setcolor(device, color, dryrun, verbose):
+def set_color(device, color, dryrun, verbose):
 	if not isinstance(color, Color):
 		color = Color(color)
 
-	rgbcolor = color.hex_l
+	RGBColor = color.hex_l
 	if not dryrun:
-		device['blink_device'].morph(hex=rgbcolor, duration=59000, steps=60) # Set our Blinkstick to morph from one step to another over the next 59 seconds
+		device['blink_device'].morph(hex=RGBColor, duration=59000, steps=60) # Set our Blinkstick to morph from one step to another over the next 59 seconds
 
 	if verbose:
-		print("Device '{0}' set color to {1}".format(device['name'], rgbcolor))
+		print("Device '{0}' set color to {1}".format(device['name'], RGBColor))
 
 	return True
 
-def turnOff(device, dryrun, verbose):
+def turn_off(device, dryrun, verbose):
 	if not dryrun:
 		device['blink_device'].turn_off()
 
@@ -100,8 +100,8 @@ def turnOff(device, dryrun, verbose):
 
 	return True
 
-def setcolorfromschedule(configpath, dryrun, time, ignorefailed, verbose):
-	config = loadschedule(configpath)
+def set_color_from_schedule(configpath, dryrun, time, ignorefailed, verbose):
+	config = load_schedule(configpath)
 
 	if not time:
 		time = datetime.datetime.now()
@@ -109,42 +109,42 @@ def setcolorfromschedule(configpath, dryrun, time, ignorefailed, verbose):
 		time = datetime.datetime.strptime(time, "%H:%M").time()
 
 	time = time.hour*60 + time.minute
-	entries = getcurrentscheduleentries(config, time)
+	entries = get_current_schedule_entries(config, time)
 
 	for entry in entries:
 		devices = []
 		for device in config['devices']:
 			if "all" in entry['devices'] or device['name'] in entry['devices']:
 				if not dryrun:
-					blink_device = blinkstick.find_by_serial(device['serial'])
-					if blink_device is None:
+					blinkDevice = blinkstick.find_by_serial(device['serial'])
+					if blinkDevice is None:
 						print("Couldn't find device '{0}' by serial number ({1})".format(device['name'], device['serial']))
 						if not ignorefailed:
 							sys.exit()
 						else:
 							continue
 				else:
-					blink_device = "dryrun socket"
-				new_device = dict(device)
-				new_device['blink_device'] = blink_device
-				devices.append(new_device)
+					blinkDevice = "dryrun socket"
+				newDevice = dict(device)
+				newDevice['blink_device'] = blinkDevice
+				devices.append(newDevice)
 
 		if "color" in entry:
 			for device in devices:
-				setcolor(device, entry['color'], dryrun, verbose)
+				set_color(device, entry['color'], dryrun, verbose)
 		elif "gradient" in entry:
 			steps = (entry['end'] - entry['start'] + 1)
 			position = (time - entry['start'])
-			colorgradient = lineargradient(entry['gradient']['start'], entry['gradient']['end'], steps)
+			colorGradient = linear_gradient(entry['gradient']['start'], entry['gradient']['end'], steps)
 			if 'type' in entry:
 				if entry['type'] == 'natural':
-					colorgradient = naturalgradient(entry['gradient']['start'], entry['gradient']['end'], steps)
+					colorGradient = natural_gradient(entry['gradient']['start'], entry['gradient']['end'], steps)
 			for device in devices:
-				setcolor(device, colorgradient[position], dryrun, verbose)
+				set_color(device, colorGradient[position], dryrun, verbose)
 		elif "state" in entry:
 			if not entry['state']: # on/yes/true in yaml are equivalent to a boolean True
 				for device in devices:
-					turnOff(device, dryrun, verify)
+					turn_off(device, dryrun, verify)
 
 def main():
 	parser = argparse.ArgumentParser(description="Run a light schedule for one or more LED controllers. This is intended to run as often as you'd like via a cron job.")
@@ -155,7 +155,7 @@ def main():
 	parser.add_argument("--verbose", "-v", help="Be more verbose", action="store_true")
 	args = parser.parse_args()
 
-	setcolorfromschedule(args.config, args.dry_run, args.fudge_time, args.ignore_failed, args.verbose)
+	set_color_from_schedule(args.config, args.dry_run, args.fudge_time, args.ignore_failed, args.verbose)
 
 if __name__=='__main__':
 	sys.exit(main())
